6acf9e7403c8671356e86bc95a5256a98808d6af
gunishakamra <gk989980@wcupa.edu>
2024-09-05 17:49:03 -0400

    Completed hw1

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -4,6 +4,14 @@ public class Q4ReverseString {
 
     public static String reverse(String s) {
         // TODO: Implement the recursive function to reverse the string
-        return "";
+        if(s.isEmpty()){
+        return s;
     }
+    return reverse(s.substring(1)) + s.charAt(0);
+}
+public static void main(String[] args) {
+    String s = "test string";
+    String reversedStr = reverse(s);
+    System.out.println(reversedStr);
+}
 }
